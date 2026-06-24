69c67d4cb155b9f8826750cbd5392493ec1e0d96
Gabriel dos Santos Silva Coelho <gabriel.coelho@sga.pucminas.br>
2026-06-24 14:18:38 -0300

    configurando corcs

--- backend/coinwise/src/main/java/backend/coinwise/config/GlobalConfig.java
+++ backend/coinwise/src/main/java/backend/coinwise/config/GlobalConfig.java
@@ -14,7 +14,10 @@ public class GlobalConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                            "http://localhost:5173",
+                            "https://sistema-moeda-estudantil-omega.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
