730ad7fd44157079a185cc889b1d32be7f524718
Gabriel dos Santos Silva Coelho <gabriel.coelho@sga.pucminas.br>
2026-06-24 14:52:49 -0300

    permissao alterado

--- backend/coinwise/src/main/java/backend/coinwise/config/GlobalConfig.java
+++ backend/coinwise/src/main/java/backend/coinwise/config/GlobalConfig.java
@@ -14,9 +14,9 @@ public class GlobalConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
+                        .allowedOriginPatterns(
                             "http://localhost:5173",
-                            "https://sistema-moeda-estudantil-omega.vercel.app"
+                            "https://*.vercel.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
